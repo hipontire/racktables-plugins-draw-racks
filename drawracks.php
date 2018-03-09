1516f23f38dbe41e464e9d8b5548cec5844448da
mineko <i.am.sanski@gmail.com>
2018-03-09 09:58:19 +0900

    セッション関数を修正

--- drawracks.php
+++ drawracks.php
@@ -2,6 +2,7 @@
 // Draw Racks to Excel
 // 
 // 2016-04-01 - Hipontire <miyoshi@outlook.com>
+// 2018-03-09 - @sesstion_start function is used instead of the sessionStart function
 
 $tab['reports']['rack'] = 'DrawRacks';				// The title of the report tab
 $tabhandler['reports']['rack'] = 'renderDrawRacks';	// register a report rendering function
@@ -13,15 +14,21 @@ require_once dirname(__FILE__) . '/drawracks/drawRacksLib.php';
 function renderDrawRacks()
 {
 	global $drawracks_conf;
-
 	$rp = new DrawRacks();
-	if ( isset($_GET['xlsx']) ) {
-		$rp->output_excelfile();
-		exit(0);
+	if ( isset($_GET['xlsx']) ){
+		if ( strlen($_COOKIE['rack_ids']) > 0) {
+			$rp->output_excelfile();
+			exit(0);
+		}
+		else
+		{
+			showWarning ("None of the racks is selected.");
+		}
 	}
 
 	// Handle the location filter
-	startSession();
+	//startSession();
+	@session_start();
 	if (isset ($_REQUEST['changeLocationFilter']))
 		unset ($_SESSION['locationFilter']);
 	if (isset ($_REQUEST['location_id']))
